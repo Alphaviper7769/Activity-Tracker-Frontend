cd480175ac97f61e84cd34366e98a549d00c48e3
Aayush Raturi <77016972+Alphaviper7769@users.noreply.github.com>
2024-05-11 10:18:14 +0530

    fix?

--- src/components/Switcher.jsx
+++ src/components/Switcher.jsx
@@ -1,6 +1,6 @@
 // Switcher.js
 import React from "react";
-import { Route, BrowserRouter, Routes } from "react-router-dom";
+import { Route, HashRouter, Routes } from "react-router-dom";
 import Dashboard from "./DashBoard/DashBoard";
 import BlacklistComponent from "./BlackList/BlackList";
 import Login from "./Login";
@@ -10,7 +10,7 @@ import NavBar from "./NavBar/NavBar";
 
 const Switcher = () => {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/login" element={<Login />} />
 
@@ -27,7 +27,7 @@ const Switcher = () => {
         <Route path="/notification" element={<Notification />} />
         {/* Add more routes here for other components */}
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 };
 
